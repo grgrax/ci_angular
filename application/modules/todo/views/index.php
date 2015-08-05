397be81e3eaf3d9661d249e616a80d16c7dd528d
deep <raxizel@gmail.com>
2015-08-05 22:24:25 +0545

    angular form validation

--- application/modules/todo/views/index.php
+++ application/modules/todo/views/index.php
@@ -12,7 +12,7 @@
     </div>
 
     <div class="panel-body">
-        <form role="form" ng-submit="addArticle()">
+        <form role="form">
             <h4>
                 Filter 
             </h4>
@@ -41,60 +41,78 @@
                 </div>                
             </span> 
         </form>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th class="center">#</th>
-                    <th>Name</th>
-                    <th>Status</th>
-                    <th>Created at</th>
-                    <th>Publish</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr ng-repeat="article in articles | filter:filter_name | filter:filter_content | filter:filter_date | orderBy : 'name'">
-                    <td>{{$index+1}}</td>
-                    <td>{{article.name | uppercase}}</td>
-                    <td>
-                        <span ng-if="article.status==1" class="label label-table label-warning">
-                            Unpublished
-                        </span>
-                        <span ng-if="article.status==2" class="label label-table label-success">
-                            Published
-                        </span>
-                        <span ng-if="article.status==3" class="label label-table label-danger">
-                            Deleted
-                        </span>
-                        <!-- <span class="label label-table label-<?=$class?>"><?=$status?></span> -->
-                    </td>
-                    <td>{{article.created_at}}</td>
-                    <td style="text-align:center">
-                        <span ng-if="article.status!=3">
-                            <input class="todo" type="checkbox" 
-                            ng-cha
-                            nge="publisharticle(articles[$index])"
-                            ng-model="articles[$index].status" 
-                            ng-true-value="'2'" 
-                            ng-false-value="'1'">
-                        </span>                    
-                    </td>
-                    <td style="text-align:center">
-                        <span ng-if="article.status==2">
-                            <a class="btn btn-xs btn-default"
-                            data-toggle="modal" 
-                            data-target="#edit_form" 
-                            ng-click="loadArticle(article.slug)"
-                            >Edit</a>
-                            &nbsp;
-                            <a class="btn btn-xs btn-danger" 
-                            ng-click="removeArticle(articles[$index])">Delete</a>
-                        </span>
-                    </td>                    
-                </tr>
-            </tbody>
-        </table>
-    </div>
+       <!--  <form role="form" ng-submit="addArticle()" name="add_article_form">
+            <div class="form-group">
+                <input type="text" class="form-control" name="name" 
+                ng-model="name" placeholder="Name here" required>
+                <span style="color:red" ng-show="add_article_form.name.$dirty && add_article_form.name.$invalid">
+                  <span ng-show="add_article_form.name.$error.required">Name is required.</span>
+              </span>
+          </div>    
+          <div class="form-group">
+            <textarea name="content" class="form-control" id="" cols="30" rows="4" 
+            placeholder="Content here"
+            ng-model="content" required></textarea>
+            <span style="color:red" ng-show="add_article_form.content.$dirty && add_article_form.content.$invalid">
+                <span ng-show="add_article_form.content.$error.required">Content is required.</span>
+            </span>
+        </div>
+        <button type="submit" class="btn btn-primary" name="add">Add</button>
+    </form>    -->
+    <table class="table">
+        <thead>
+            <tr>
+                <th class="center">#</th>
+                <th>Name</th>
+                <th>Status</th>
+                <th>Created at</th>
+                <th>Publish</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr ng-repeat="article in articles | filter:filter_name | filter:filter_content | filter:filter_date | orderBy : 'name'">
+                <td>{{$index+1}}</td>
+                <td>{{article.name | uppercase}}</td>
+                <td>
+                    <span ng-if="article.status==1" class="label label-table label-warning">
+                        Unpublished
+                    </span>
+                    <span ng-if="article.status==2" class="label label-table label-success">
+                        Published
+                    </span>
+                    <span ng-if="article.status==3" class="label label-table label-danger">
+                        Deleted
+                    </span>
+                    <!-- <span class="label label-table label-<?=$class?>"><?=$status?></span> -->
+                </td>
+                <td>{{article.created_at}}</td>
+                <td style="text-align:center">
+                    <span ng-if="article.status!=3">
+                        <input class="todo" type="checkbox" 
+                        ng-cha
+                        nge="publisharticle(articles[$index])"
+                        ng-model="articles[$index].status" 
+                        ng-true-value="'2'" 
+                        ng-false-value="'1'">
+                    </span>                    
+                </td>
+                <td style="text-align:center">
+                    <span ng-if="article.status==2">
+                        <a class="btn btn-xs btn-default"
+                        data-toggle="modal" 
+                        data-target="#edit_form" 
+                        ng-click="loadArticle(article.slug)"
+                        >Edit</a>
+                        &nbsp;
+                        <a class="btn btn-xs btn-danger" 
+                        ng-click="removeArticle(articles[$index])">Delete</a>
+                    </span>
+                </td>                    
+            </tr>
+        </tbody>
+    </table>
+</div>
 </div>
 
 
@@ -115,40 +133,13 @@
             </div>
             <div class="modal-body">
                 <div class="form-group center-block">
-                 <form role="form" ng-submit="addArticle()">
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="name" 
-                        ng-model="article.name" placeholder="Name here" required
-                        value="{{article.name}}">
-                        name : {{article.name}}
-
-                    </div>    
-                    <div class="form-group">
-                        <textarea name="content" class="form-control" id="" cols="30" rows="4" 
-                        placeholder="Content here"
-                        ng-model="content" required></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary" name="add">Add</button>
-                </form>
-            </div>                      
-        </div>
-    </div>
-</div>
-</div>
-
-<div class="modal fade" id="add_form" tabindex="-1" role="dialog" aria-labelledby="edit_form" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content center">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h3 class="modal-title">Add Article</h3>
-            </div>
-            <div class="modal-body">
-                <div class="form-group center-block">
-                    <form role="form" ng-submit="editArticle(articles[$index])">
+                    <form role="form" ng-submit="editArticle()">
                         <div class="form-group">
                             <input type="text" class="form-control" name="name" 
-                            ng-model="name" placeholder="Name here" required>
+                            ng-model="article.name" placeholder="Name here" required
+                            value="{{article.name}}">
+                            name : {{article.name}}
+
                         </div>    
                         <div class="form-group">
                             <textarea name="content" class="form-control" id="" cols="30" rows="4" 
@@ -161,6 +152,39 @@
             </div>
         </div>
     </div>
+</div>
+
+<div class="modal fade" id="add_form" tabindex="-1" role="dialog" aria-labelledby="add_form" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content center">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h3 class="modal-title">Add Article</h3>
+            </div>
+            <div class="modal-body">
+                <div class="form-group center-block">
+                    <form role="form" ng-submit="addArticle()" name="add_article_form" >
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="name" 
+                            ng-model="name" placeholder="Name here" required>
+                            <span style="color:red" ng-show="add_article_form.name.$dirty && add_article_form.name.$invalid">
+                              <span ng-show="add_article_form.name.$error.required">Name is required.</span>
+                          </span>
+                      </div>    
+                      <div class="form-group">
+                        <textarea name="content" class="form-control" id="" cols="30" rows="4" 
+                        placeholder="Content here"
+                        ng-model="content" required></textarea>
+                        <span style="color:red" ng-show="add_article_form.content.$dirty && add_article_form.content.$invalid">
+                            <span ng-show="add_article_form.content.$error.required">Content is required.</span>
+                        </span>
+                    </div>
+                    <button type="submit" class="btn btn-primary" name="add">Add</button>
+                </form>            
+            </div>                      
+        </div>
+    </div>
+</div>
 </div>
 
 
